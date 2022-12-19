62e2c6ecc595b8616830bddeb98162be347777fc
DarioCastel <lecastelnovo@gmail.com>
2022-12-19 20:00:05 -0300

    galeria acordeon

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="acordeon">
+        <span className="img"></span>
+        <span className="img"></span>
+        <span className="img"></span>
+        <span className="img"></span>
+        <span className="img"></span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
